8b23741a537b017dc98f23ae296cabb2e0c6dd48
Ayse Selma Tufan <150919460+Selma34568@users.noreply.github.com>
2024-01-28 01:05:03 +0100

    Update _game.py

--- _game.py
+++ _game.py
@@ -1,129 +1,110 @@
+import random
 
-
-
-def play_qame(questions):
+def play_game():
     print("Welcome to our general culture game")
 
-score = 0
-lives = 3
 
-questions_answer = [
-    {'question': 'What is the largest continent in the world?',
-     'options': ['Europe', 'Asian continent', 'Africa', 'Antarctica'],
-     'answer': 'Asian continent'}
-]
+    user_name = input("What's your name? ")
 
-for question in questions_answer: 
-    user_input = input(f"{question['question']} ({', '.join(question['options'])}): ").lower()
+    game_theme = input("Choose a game theme (e.g., history, geography, science , all themes): ")
 
-    if user_input == question['answer'].lower():
-        score += 5
-        print("Great, user answer is correct")
+    score = 0
+    lives = 3
+
+    if game_theme.lower() == 'geography':
+        questions_answer = [
+            {'question': 'Which country is in the Asian continent?',
+             'options': ['Madagascar', 'Peru', 'Singapore'],
+             'answer': 'Singapore'},
+            {'question': 'What is the largest continent in the world?',
+             'options': ['Europe', 'Asian continent', 'Africa', 'Antarctica'],
+             'answer': 'Asian continent'},
+            {'question': 'How many countries are in the world?',
+             'options': ['200', '250', '275', '100'],
+             'answer': '250'}
+        ]
+
+    elif game_theme.lower() == 'history':
+        questions_answer = [
+            {'question': 'In what year did the first world war occur?',
+             'options': ['1915', '1936', '1914', '1923'],
+             'answer': '1914'},
+            {'question': 'Who is the author of the novel "Crime and Punishment?',
+             'options': ['Leo Tolstoy', 'Fyodor Dostoyevsky', 'Anton Chekhov', 'Ivan Turgenev'],
+             'answer': 'Fyodor Dostoyevsky'},
+            {'question': 'Who set foot on the moon for the first time?',
+             'options': ['Elon Musk', 'Neil Armstrong', 'Yuri Gagarin', 'Louis Armstrong'],
+             'answer': 'Neil Armstrong'}
+        ]
+    elif game_theme.lower() == 'science':
+        questions_answer = [
+            {'question': 'What is the chemical symbol for water?',
+             'options': ['H2O', 'CO2', 'O2', 'N2'],
+             'answer': 'H2O'},
+            {'question': 'What are the first few decimal places of pi?',
+             'options': ['3.1415926535...', '3.1515926535...', '3.1615926535...', '3.1715926535...'],
+             'answer': '3.1415926535...'},
+            {'question': 'What is the largest planet in the Solar System?',
+             'options': ['Jupiter', 'Uranus', 'Neptune', 'Mercury'],
+             'answer': 'Jupiter'}
+        ]
+    elif game_theme.lower() == 'all themes':
+        questions_answer = [
+            {'question': 'Which country is in the Asian continent?',
+             'options': ['Madagascar', 'Peru', 'Singapore'],
+             'answer': 'Singapore'},
+            {'question': 'What is the largest continent in the world?',
+             'options': ['Europe', 'Asian continent', 'Africa', 'Antarctica'],
+             'answer': 'Asian continent'},
+            {'question': 'How many countries are in the world?',
+             'options': ['200', '250', '275', '100'],
+             'answer': '250'},
+            {'question': 'In what year did the first world war occur?',
+             'options': ['1915', '1936', '1914', '1923'],
+             'answer': '1914'},
+            {'question': 'Who is the author of the novel "Crime and Punishment?',
+             'options': ['Leo Tolstoy', 'Fyodor Dostoyevsky', 'Anton Chekhov', 'Ivan Turgenev'],
+             'answer': 'Fyodor Dostoyevsky'},
+            {'question': 'Who set foot on the moon for the first time?',
+             'options': ['Elon Musk', 'Neil Armstrong', 'Yuri Gagarin', 'Louis Armstrong'],
+             'answer': 'Neil Armstrong'},
+            {'question': 'What is the chemical symbol for water?',
+             'options': ['H2O', 'CO2', 'O2', 'N2'],
+             'answer': 'H2O'},
+            {'question': 'What are the first few decimal places of pi?',
+             'options': ['3.1415926535...', '3.1515926535...', '3.1615926535...', '3.1715926535...'],
+             'answer': '3.1415926535...'},
+            {'question': 'What is the largest planet in the Solar System?',
+             'options': ['Jupiter', 'Uranus', 'Neptune', 'Mercury'],
+             'answer': 'Jupiter'}
+
+        ]
     else:
-        print('Unfortunately, user answer is wrong')
-        lives -= 1
-        print(f'You have {lives} lives remaining.\n')
+        print("Invalid theme. Exiting the game.")
+        return
 
-    if lives == 0:
-        print('Game over! Your score is:', score)
-        break
+    random.shuffle(questions_answer)
 
+    for question in questions_answer:
+        user_input = input(f"{question['question']} ({', '.join(question['options'])}): ").lower()
 
-score = 0
-lives = 3
+        if user_input == question['answer'].lower():
+            score += 5
+            print("Great, your answer is correct!")
+        else:
+            print('Unfortunately, your answer is wrong')
+            lives -= 1
+            print(f'You have {lives} lives remaining.\n')
 
-questions_answer = [
-    {'question': 'How many countries are in the world?',
-     'options': ['200', '250', '275', '100'],
-     'answer': '250'}
-]
+        if lives == 0:
+            print(f"Game over, {user_name}! Your final score is: {score}")
+            break
+        elif lives > 0:
+            print(f'Your score is: {score}')
 
-for question in questions_answer:
-    user_input = input(f"{question['question']} ({', '.join(question['options'])}): ").lower()
+    total_score = score * 5  # 5 points for each correct answer
+    print(f'Thanks for playing, {user_name}!')
+    print(f'Your total score is: {total_score}')
 
-    if user_input == question['answer'].lower():
-        score += 5
-        print("Great, user answer is correct")
-    else:
-        print('Unfortunately, user answer is wrong')
-        lives -= 1
-        print(f'You have {lives} lives remaining.\n')
-
-    if lives == 0:
-        print('Game over! Your score is:', score)
-        break
-
-
-
-score = 0
-lives = 3
-
-questions_answer = [
-    {'question': 'What is the smallest country in the world??',
-     'options': ['Monako','Vatikan','Nauru','Tuvalu'],
-     'answer': 'Vatikan'}
-]
-
-for question in questions_answer:
-    user_input = input(f"{question['question']} ({', '.join(question['options'])}): ").lower()
-
-    if user_input == question['answer'].lower():
-        score += 5
-        print("Great, user answer is correct")
-    else:
-        print('Unfortunately, user answer is wrong')
-        lives -= 1
-        print(f'You have {lives} lives remaining.\n')
-
-    if lives == 0:
-        print('Game over! Your score is:', score)
-        break
-
-
-
-score = 0
-lives = 3
-
-questions_answer = [
-    {'question': 'What is the capital city England?',
-     'options': ['Paris','London','Warsaw','Berlin'],
-     'answer': 'London'}
-]
-
-for question in questions_answer:
-    user_input = input(f"{question['question']} ({', '.join(question['options'])}): ").lower()
-
-    if user_input == question['answer'].lower():
-        score += 5
-        print("Great, user answer is correct")
-    else:
-        print('Unfortunately, user answer is wrong')
-        lives -= 1
-        print(f'You have {lives} lives remaining.\n')
-
-    if lives == 0:
-        print('Game over! Your score is:', score)
-        break
-
-
-
-
-questions_answer = [
-    {'question': 'What is the largest continent in the world?',
-     'options': ['Europe', 'Asia', 'Africa', 'Antarctica'],
-     'answer': 'Asia'},
-
-    {'question': 'How many countries are in the world?',
-     'options': ['200', '250', '275', '100'],
-     'answer': '250'},
-
-    {'question': 'What is the smallest country in the world?',
-     'options': ['Monaco', 'Vatican', 'Nauru', 'Tuvalu'],
-     'answer': 'Vatikan'},
-
-    {'question': 'What is the capital city of England?',
-     'options': ['Paris', 'London', 'Warsaw', 'Berlin'],
-     'answer': 'London'},
-
-]
+play_game()
 
